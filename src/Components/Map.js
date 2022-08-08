26dda507e0f20c2a4b6a897850868f3b6a77042e
hakim hassani <hakimhassani97@gmail.com>
2022-08-08 18:20:16 +0200

    Added dat

---
+++ src/Components/Map.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import {
+	MapContainer, TileLayer, Marker, Popup, useMapEvents,
+} from 'react-leaflet';
+import Path from './Path';
+import path1 from '../Data/Data';
+
+function LocationMarker() {
+	const [position, setPosition] = useState(null);
+
+	const map = useMapEvents({
+		click() {
+			map.locate();
+		},
+		locationfound(e) {
+			setPosition(e.latlng);
+			map.flyTo(e.latlng, map.getZoom());
+		},
+	});
+
+	return position === null ? null : (
+		<Marker position={position}>
+			<Popup>You are here</Popup>
+		</Marker>
+	);
+}
+
+function Map() {
+	const center = {
+		lat: 48.78166783486849,
+		lng: 2.2153474224385263,
+	};
+
+	return (
+		<MapContainer
+			style={{ width: '100%', height: '100vh' }}
+			center={center}
+			zoom={17}
+			scrollWheelZoom
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			{/* <LocationMarker /> */}
+			<Path positions={[...path1]} />
+		</MapContainer>
+	);
+}
+
+export default Map;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const DefaultIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow,
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
